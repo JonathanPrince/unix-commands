3f18c3de1b7b9aeeebd6569705ff38a45c93e825
Jonathan Prince <jonathan.prince@gmail.com>
2016-10-15 23:10:11 +0200

    super basic implementation

--- mkdir/mkdir.c
+++ mkdir/mkdir.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <sys/stat.h>
 
+int main(int argc, char const *argv[]) {
+  int i;
+  if (argc == 1) {
+    fprintf(stderr, "Directory name was not specified.\n");
+  }
+  for (i = 0; i < argc; ++i) {
+    mkdir(argv[i], 0777);
+  }
+  return 0;
+}
