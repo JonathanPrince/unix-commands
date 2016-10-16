cd61c3437c3903caffc2d2b5a1267546f93c2457
Jonathan Prince <jonathan.prince@gmail.com>
2016-10-16 22:24:55 +0200

    add error handling

--- mkdir/mkdir.c
+++ mkdir/mkdir.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include <sys/stat.h>
 
 int main(int argc, char const *argv[]) {
-  int i;
+  int result = 0;
+  int i = 1;
+
   if (argc == 1) {
     fprintf(stderr, "Directory name was not specified.\n");
+    result = -1;
   }
-  for (i = 0; i < argc; ++i) {
-    mkdir(argv[i], 0777);
+
+  while (i < argc) {
+    result = mkdir(argv[i], 0777);
+
+    if (result != 0) {
+      fprintf(stderr, "Error: '%s' %s\n", argv[i], strerror(errno));
+    }
+    i++;
   }
-  return 0;
+
+  return result;
 }
